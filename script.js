88c108b3571e13da42d37913fea6b7f877d60a26
DeltaBaby <jaredstonemercado@gmail.com>
2024-10-01 02:33:28 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slide');
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.classList.remove('active');
+        slide.style.opacity = '0';
+        if (i === index) {
+            slide.classList.add('active');
+            slide.style.opacity = '1';
+        }
+    });
+}
+
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+}
+
+setInterval(nextSlide, 5000); // Change slide every 5 seconds
